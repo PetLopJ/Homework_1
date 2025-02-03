7ab6ce5830af8e5ade0ed12216574c2354b6d8ed
PetLopJ <Peterjlopez0911@gmail.com>
2025-02-03 10:40:24 -0500

    added two classes with primary constructors

--- app/src/main/java/com/example/homework1/Student.kt
+++ app/src/main/java/com/example/homework1/Student.kt
@@ -1,4 +1,8 @@
 package com.example.homework1
 
-class Student {
+class Student constructor(var name:String, var major:String, var courses:List<courseInfo>) {
+
+    fun totalCredits():Int {
+        //calculates the total credit from the list of courses taken and returns that value
+    }
 }
--- app/src/main/java/com/example/homework1/courseInfo.kt
+++ app/src/main/java/com/example/homework1/courseInfo.kt
@@ -1,3 +1,6 @@
 package com.example.homework1
 
-data class courseInfo()
+data class courseInfo(var courseName:String, var startTime:String, var endTime:String, var days:String, var credit:Int) {
+
+
+}
