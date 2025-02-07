5daf89c0d4001fb6f02789b10d5ec2929f5f0d6b
PetLopJ <peterjlopez0911@gmail.com>
2025-02-06 20:16:15 -0500

    commit

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,72 +28,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var student = Student("john", "fkf")
 
-            var s1 = Student("John","CPIS")
+            Surface {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding).fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally)   {
+                    Modifier.padding(20.dp).shadow(30.dp)
+                    RoundedCornerShape(10.dp)
 
-            Heading(h = "Heading")
-            NormalText(t = "Normal Text")
-
-            Surface (
-                modifier = Modifier.fillMaxWidth().padding(40.dp),
-                shape = RoundedCornerShape(10.dp),
-                shadowElevation = 10.dp,
-            ) {
-                Column {
-                    ShowStudent(s1)
-                }
-            }
-
-//            Homework1Theme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
+                        MainScreen(Modifier.padding(innerPadding).fillMaxSize(),"Heading", "Normal Text")
+                        StudentDisplay(student)
+                    }
                 }
             }
         }
-
-//    }
-//}
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    Homework1Theme {
-//        Greeting("Android")
-//    }
-//}
-
-// Homework 1 composable functions:
+    }
+}
 
 @Composable
-fun Heading(h:String) {
-    Text(text = h, color = (MaterialTheme.colorScheme.primary),
+fun MainScreen(modifier:Modifier, heading:String, text:String) {
+    Text(text = heading, color = (MaterialTheme.colorScheme.primary),
         fontWeight = (FontWeight.Bold),
         style = (MaterialTheme.typography.headlineMedium))
+    Text(text = text , color = (Color.Magenta), style = (MaterialTheme.typography.bodyLarge))
 }
 
 @Composable
-fun NormalText(t:String) {
-    Text(text = t,
-        color = (Color.Magenta),
-        style = (MaterialTheme.typography.headlineMedium))
+fun StudentDisplay(student:Student) {
+    Text(text = student.name)
+    Text(text = student.major)
 }
 
 @Composable
-fun ShowStudent(s:Student) {
-
-   println(s.name)
-    println(s.major)
+fun CourseDisplay(course:String, start:String, end:String) {
 
 }
 
--- app/src/main/java/com/example/homework1/courseInfo.kt
+++ app/src/main/java/com/example/homework1/courseInfo.kt
@@ -1,6 +1,14 @@
 package com.example.homework1
 
+import androidx.compose.runtime.Composable
+
 data class courseInfo(var courseName:String, var startTime:String, var endTime:String, var days:String, var credit:Int) {
 
+    var course = courseName
+    var start = startTime
+    var end = endTime
 
+    val display =
+        
+        CourseDisplay(course, start, end)
 }
